fdf98b617eb0b72264af2dae0a462af4d3567707
Erik Boasson <erik.boasson@prismtech.com>
2017-06-20 12:09:49 +0200

    Initial Commit

---
+++ zenop/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  zenop
+//
+//  Created by Erik Boasson on 20-06-2017.
+//  Copyright © 2017 ADLINK ATO. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
