dc87250a0036e050ea45619f1e44038f65ecd9f4
Erik Boasson <erik.boasson@adlinktech.com>
2018-02-13 21:10:31 +0100

    let "simpleset" insert/delete return something interesting and gracefully handle duplicates, full sets, &c.

--- src/zhe-simpleset.h
+++ src/zhe-simpleset.h
@@ -3,6 +3,12 @@
 
 #include "zhe-package.h"
 
+typedef enum simpleset_insert_result {
+    SSIR_EXISTS,
+    SSIR_NOSPACE,
+    SSIR_SUCCESS
+} simpleset_insert_result_t;
+
 #define MAKE_SIMPLESET_SPEC_type(linkage_, name_, key_type_, type_, index_type_, max_elems_) \
     typedef struct name_ {                                              \
         index_type_ count;                                              \
@@ -28,10 +34,10 @@
     linkage_ index_type_ name_##_count(const name_##_t *it);
 
 #define MAKE_SIMPLESET_SPEC_insert(linkage_, name_, key_type_, type_, index_type_, max_elems_) \
-    linkage_ void name_##_insert(name_##_t *set, type_ elem); \
+    linkage_ simpleset_insert_result_t name_##_insert(name_##_t *set, type_ elem); \
 
 #define MAKE_SIMPLESET_SPEC_delete(linkage_, name_, key_type_, type_, index_type_, max_elems_) \
-    linkage_ void name_##_delete(name_##_t *set, type_ elem); \
+    linkage_ bool name_##_delete(name_##_t *set, type_ elem); \
 
 #define MAKE_SIMPLESET_SPEC_iter_init(linkage_, name_, key_type_, type_, index_type_, max_elems_) \
     linkage_ void name_##_iter_init(name_##_iter_t *it, const name_##_t *set); \
@@ -65,7 +71,7 @@
     }
 
 #define MAKE_SIMPLESET_BODY_init(linkage_, name_, key_type_, type_, index_type_, index_type_sub_, cmp_, key_from_elem_, max_elems_) \
-    linkage_ void name_##_init(name_##_t *set)       \
+    linkage_ void name_##_init(name_##_t *set)                          \
     {                                                                   \
         set->count index_type_sub_ = 0;                                 \
     }
@@ -74,48 +80,50 @@
     linkage_ bool name_##_contains(const name_##_t *set, key_type_ elem) \
     {                                                                   \
         index_type_ pos;                                                \
-        return name_##_search(set, elem, &pos);         \
+        return name_##_search(set, elem, &pos);                         \
     }
 
 #define MAKE_SIMPLESET_BODY_count(linkage_, name_, key_type_, type_, index_type_, index_type_sub_, cmp_, key_from_elem_, max_elems_) \
-    linkage_ index_type_ name_##_count(const name_##_t *set) \
+    linkage_ index_type_ name_##_count(const name_##_t *set)            \
     {                                                                   \
         return set->count;                                              \
     }
 
 #define MAKE_SIMPLESET_BODY_insert(linkage_, name_, key_type_, type_, index_type_, index_type_sub_, cmp_, key_from_elem_, max_elems_) \
-    linkage_ void name_##_insert(name_##_t *set, type_ elem) \
+    linkage_ simpleset_insert_result_t name_##_insert(name_##_t *set, type_ elem) \
     {                                                                   \
         index_type_ pos;                                                \
-        bool present;                                                   \
-        zhe_assert(set->count index_type_sub_ < max_elems_);            \
-        present = name_##_search(set, key_from_elem_(elem), &pos); \
-        zhe_assert(!present);                                           \
-        (void)present;                                                  \
-        if (pos index_type_sub_ < set->count index_type_sub_) {         \
-            memmove(&set->elems[pos index_type_sub_+1],                 \
-                    &set->elems[pos index_type_sub_],                   \
-                    (set->count index_type_sub_ - pos index_type_sub_) * sizeof(set->elems[0])); \
+        if (name_##_search(set, key_from_elem_(elem), &pos)) {          \
+            return SSIR_EXISTS;                                         \
+        } else if (set->count index_type_sub_ == max_elems_) {          \
+            return SSIR_NOSPACE;                                        \
+        } else {                                                        \
+            if (pos index_type_sub_ < set->count index_type_sub_) {     \
+                memmove(&set->elems[pos index_type_sub_+1],             \
+                        &set->elems[pos index_type_sub_],               \
+                        (set->count index_type_sub_ - pos index_type_sub_) * sizeof(set->elems[0])); \
+            }                                                           \
+            set->elems[pos index_type_sub_] = elem;                     \
+            set->count index_type_sub_ = set->count index_type_sub_ + 1; \
+            return SSIR_SUCCESS;                                        \
         }                                                               \
-        set->elems[pos index_type_sub_] = elem;                         \
-        set->count index_type_sub_ = set->count index_type_sub_ + 1;    \
     }
 
 #define MAKE_SIMPLESET_BODY_delete(linkage_, name_, key_type_, type_, index_type_, index_type_sub_, cmp_, key_from_elem_, max_elems_) \
-    linkage_ void name_##_delete(name_##_t *set, type_ elem) \
+    linkage_ bool name_##_delete(name_##_t *set, type_ elem) \
     {                                                                   \
         index_type_ pos;                                                \
-        bool present;                                                   \
-        zhe_assert(set->count index_type_sub_ < max_elems_);            \
-        present = name_##_search(set, key_from_elem_(elem), &pos); \
-        zhe_assert(present);                                            \
-        (void)present;                                                  \
-        if (pos index_type_sub_ + 1 < set->count index_type_sub_) {     \
-            memmove(&set->elems[pos index_type_sub_],                   \
-                    &set->elems[pos index_type_sub_+1],                 \
-                    (set->count index_type_sub_ - pos index_type_sub_ - 1) * sizeof(set->elems[0])); \
+        if (!name_##_search(set, key_from_elem_(elem), &pos)) {         \
+            return false;                                               \
+        } else {                                                        \
+            if (pos index_type_sub_ + 1 < set->count index_type_sub_) { \
+                memmove(&set->elems[pos index_type_sub_],               \
+                        &set->elems[pos index_type_sub_+1],             \
+                        (set->count index_type_sub_ - pos index_type_sub_ - 1) * sizeof(set->elems[0])); \
+            }                                                           \
+            set->count index_type_sub_ = set->count index_type_sub_ - 1; \
+            return true;                                                \
         }                                                               \
-        set->count index_type_sub_ = set->count index_type_sub_ - 1;    \
     }
 
 #define MAKE_SIMPLESET_BODY_iter_init(linkage_, name_, key_type_, type_, index_type_, index_type_sub_, cmp_, key_from_elem_, max_elems_) \
@@ -126,7 +134,7 @@
     }
 
 #define MAKE_SIMPLESET_BODY_iter_next(linkage_, name_, key_type_, type_, index_type_, index_type_sub_, cmp_, key_from_elem_, max_elems_) \
-    linkage_ bool name_##_iter_next(name_##_iter_t *it, type_ *elem) \
+    linkage_ bool name_##_iter_next(name_##_iter_t *it, type_ *elem)    \
     {                                                                   \
         if (it->cursor index_type_sub_ < it->set->count index_type_sub_) { \
             *elem = it->set->elems[it->cursor index_type_sub_];         \
@@ -157,13 +165,13 @@
     linkage_ bool name_##_contains(const name_##_t *set, key_type_ elem);
 
 #define MAKE_SIMPLESET_ALIAS_SPEC_coount(linkage_, name_, base_name_, key_type_, type_, index_type_, base_type_, base_index_type_) \
-    linkage_ index_type_ name_##_contains(const name_##_t *set);
+    linkage_ index_type_ name_##_count(const name_##_t *set);
 
 #define MAKE_SIMPLESET_ALIAS_SPEC_insert(linkage_, name_, base_name_, key_type_, type_, index_type_, base_type_, base_index_type_) \
-    linkage_ void name_##_insert(name_##_t *set, type_ elem);
+    linkage_ simpleset_insert_result_t name_##_insert(name_##_t *set, type_ elem);
 
 #define MAKE_SIMPLESET_ALIAS_SPEC_delete(linkage_, name_, base_name_, key_type_, type_, index_type_, base_type_, base_index_type_) \
-    linkage_ void name_##_delete(name_##_t *set, type_ elem);
+    linkage_ bool name_##_delete(name_##_t *set, type_ elem);
 
 #define MAKE_SIMPLESET_ALIAS_SPEC_iter_init(linkage_, name_, base_name_, key_type_, type_, index_type_, base_type_, base_index_type_) \
     linkage_ void name_##_iter_init(name_##_iter_t *it, const name_##_t *set);
@@ -173,8 +181,8 @@
 
 
 #define MAKE_SIMPLESET_ALIAS_BODY_init(linkage_, name_, base_name_, key_type, type_, index_type_) \
-    linkage_ void name_##_init(name_##_t *set) { \
-        zhe_base_name_##_init(&set->x_##base_name_);                     \
+    linkage_ void name_##_init(name_##_t *set) {                        \
+        zhe_base_name_##_init(&set->x_##base_name_);                    \
     }
 
 #define MAKE_SIMPLESET_ALIAS_BODY_search(linkage_, name_, base_name_, key_type, type_, index_type_) \
@@ -184,34 +192,33 @@
 
 #define MAKE_SIMPLESET_ALIAS_BODY_contains(linkage_, name_, base_name_, key_type, type_, index_type_) \
     linkage_ bool name_##_contains(const name_##_t *set, key_type_ elem) { \
-        return zhe_base_name_##_contains(&set->x_##base_name_, elem); \
+        return zhe_base_name_##_contains(&set->x_##base_name_, elem);   \
     }
 
 #define MAKE_SIMPLESET_ALIAS_BODY_count(linkage_, name_, base_name_, key_type, type_, index_type_) \
-    linkage_ index_type_ name_##_contains(const name_##_t *set) { \
-        return zhe_base_name_##_count(&set->x_##base_name_); \
+    linkage_ index_type_ name_##_count(const name_##_t *set) {          \
+        return zhe_base_name_##_count(&set->x_##base_name_);            \
     }
 
 #define MAKE_SIMPLESET_ALIAS_BODY_insert(linkage_, name_, base_name_, key_type, type_, index_type_) \
-    linkage_ void name_##_insert(name_##_t *set, type_ elem) { \
-        zhe_base_name_##_insert(&set->x_##base_name_, elem);         \
+    linkage_ simpleset_insert_result_t name_##_insert(name_##_t *set, type_ elem) { \
+        return zhe_base_name_##_insert(&set->x_##base_name_, elem);     \
     }
 
 #define MAKE_SIMPLESET_ALIAS_BODY_delete(linkage_, name_, base_name_, key_type, type_, index_type_) \
-    linkage_ void name_##_delete(name_##_t *set, type_ elem) { \
-        zhe_base_name_##_delete(&set->x_##base_name_, elem);         \
+    linkage_ bool name_##_delete(name_##_t *set, type_ elem) {          \
+        return zhe_base_name_##_delete(&set->x_##base_name_, elem);     \
     }
 
 #define MAKE_SIMPLESET_ALIAS_BODY_iter_init(linkage_, name_, base_name_, key_type, type_, index_type_) \
     linkage_ void name_##_iter_init(name_##_iter_t *it, const name_##_t *set) { \
-        zhe_base_name_##_iter_init(&it, &set->x_##base_name_);       \
+        zhe_base_name_##_iter_init(&it, &set->x_##base_name_);          \
     }
 
 #define MAKE_SIMPLESET_ALIAS_BODY_iter_next(linkage_, name_, base_name_, key_type, type_, index_type_) \
-    linkage_ bool name_##_iter_next(name_##_iter_t *it, type_ *elem) { \
-        return zhe_base_name_##_iter_next(&it->x_##base_name_, elem); \
+    linkage_ bool name_##_iter_next(name_##_iter_t *it, type_ *elem) {  \
+        return zhe_base_name_##_iter_next(&it->x_##base_name_, elem);   \
     }
-
 
 #if 0
 #define CMP(a,b) ((a) == (b) ? 0 : ((a) < (b)) ? -1 : 1)
