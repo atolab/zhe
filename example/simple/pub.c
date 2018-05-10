99c2d45c4a663c369ab1e622dbd70bef870254ba
Erik Boasson <eb@ilities.com>
2018-05-10 16:27:19 +0800

    remove some extraneous code from simple pub/sub & change to communicating counter

--- example/simple/pub.c
+++ example/simple/pub.c
@@ -8,16 +8,12 @@ int main(int argc, char* argv[])
     struct zhe_platform * const platform = zhe(7447, NULL);
     zhe_pubidx_t p;
     p = zhe_publish(1, 0, 1);
-    uint64_t  delay = 1000000000;
+    uint64_t delay = 1000000000;
     zhe_once(platform, delay);
-
     uint64_t count = 0;
-    char* data = "zenoh data";
-
     while (true) {
         printf(">> Writing count %llu\n", count);
-        zhe_write(p, data, (zhe_paysize_t)strlen(data), zhe_platform_time());
-        zhe_flush(zhe_platform_time());
+        zhe_write(p, &count, sizeof(count), zhe_platform_time());
         count += 1;
         zhe_once(platform, delay);
     }
--- example/simple/sub.c
+++ example/simple/sub.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
 #include "zhe-util.h"
 #include "zhe-platform.h"
 
 void data_handler(zhe_rid_t rid, const void *payload, zhe_paysize_t size, void *vpub)
 {
-    printf(">> Processing data for resource %ju\n", (uintmax_t)rid);
-    char *data = (char*)payload;
-    printf(">> Received: \t");
-    for (int i = 0; i < size; ++i) {
-        printf("%c", data[i]);
+    if (size != 8) {
+        printf("received unexpected payload (rid %ju, size %u)\n", (uintmax_t)rid, (unsigned)size);
+    } else {
+        /* copy to avoid issues on platforms that require aligned access */
+        uint64_t count;
+        memcpy(&count, payload, sizeof(count));
+        printf("%"PRIu64"\n", count);
     }
-    printf("\n");
 }
 
 int main(int argc, char* argv[])
@@ -22,7 +25,6 @@ int main(int argc, char* argv[])
 #endif
     zhe_subidx_t s;
     s = zhe_subscribe(1, 0, 0, data_handler, NULL);
-    zhe_flush(zhe_platform_time());
     while (true) {
         zhe_dispatch(platform);
     }
